c11dd3d6dc8f7f861a4f0a8d68116753b2987aa4
cf2794 <cf2794@axon.rc.zi.columbia.edu>
2023-04-24 16:40:59 -0400

    Add shuffle transforms

--- utils.py
+++ utils.py
@@ -6,6 +6,62 @@ def to_pair(input):
         return input[:2]
     else:
         return input, input
+    
+class PhaseShuffle(object):
+    r"""
+    Shuffles phase info of image
+    """
+    def __init__(self):
+        pass
+        
+    def __call__(self, tensor):
+        
+        y = torch.fft.rfft2(tensor)
+
+        res = np.real(y)
+        ims = np.imag(y)
+        ims_shape = ims.shape
+        shuff_indices = np.arange(ims_shape[-1]*ims_shape[-2])
+        np.random.shuffle(shuff_indices)
+        for c in [1]:
+            c_vals = ims[c]
+            c_shape = c_vals.shape
+            c_vals = c_vals.flatten()
+            c_vals = c_vals[shuff_indices]
+            ims[c] = c_vals.reshape(c_shape)
+        new_image = res +ims*1j
+        return torch.fft.irfft2(new_image) 
+    
+    def __repr__(self):
+        return self.__class__.__name__ 
+    
+class MagShuffle(object):
+    r"""
+    Shuffles magnitude info of image
+    """
+    def __init__(self):
+        pass
+        
+    def __call__(self, tensor):
+        
+        y = torch.fft.rfft2(tensor)
+
+        res = np.real(y)
+        ims = np.imag(y)
+        res_shape = res.shape
+        shuff_indices = np.arange(res_shape[-1]*res_shape[-2])
+        np.random.shuffle(shuff_indices)
+        for c in [1]:
+            c_vals = res[c]
+            c_shape = c_vals.shape
+            c_vals = c_vals.flatten()
+            c_vals = c_vals[shuff_indices]
+            res[c] = c_vals.reshape(c_shape)
+        new_image = res +ims*1j
+        return torch.fft.irfft2(new_image) 
+    
+    def __repr__(self):
+        return self.__class__.__name__ 
 
 class AddGaussianNoise(object):
     r"""
